9dad626ed0ce33ff15b8647ba21152729d5432bf
partheev-boora <partheev.boora@kpipartners.com>
2022-10-18 17:59:25 +0530

    commit

--- CRUD/src/main/java/com/KPI/emp/DeptController.java
+++ CRUD/src/main/java/com/KPI/emp/DeptController.java
@@ -1,19 +1,5 @@
-package com.KPI.emp;
+package src.main.java.com.KPI.emp;
 
-import java.util.ArrayList;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
 public class DeptController {
-	@Autowired
-	private EmployeeRepo er;//up to date
-	@GetMapping("/dept") 
-	public ArrayList<String> getempname(){
-		ArrayList<String> edept=new ArrayList<String>();
-    	for(EmployeeEntity emt:er.findAll()) {
-    		edept.add(emt.getDept());
-    	}
-    	return edept;
-	}
+
 }
